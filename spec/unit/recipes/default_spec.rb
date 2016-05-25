d67ea92ab0bc7d77a075ad24c50c8d394303334e
Kristian Van Der Vliet <vanders@liqwyd.com>
2016-05-25 19:15:43 +0100

    Fix Chefspec

--- spec/unit/recipes/default_spec.rb
+++ spec/unit/recipes/default_spec.rb
@@ -8,6 +8,10 @@ require 'spec_helper'
 
 describe 'gce_gcsfuse::default' do
   context 'When all attributes are default, on Ubuntu Trusty' do
+    before do
+      stub_command('mount | grep /gcsfuse >/dev/null').and_return(true)
+    end
+
     let(:chef_run) do
       runner = ChefSpec::ServerRunner.new do |node, _server|
         node.default['lsb']['codename'] = 'trusty'
